8c181381114c587de53981da556026575989190a
stroju <stroju@gmail.com>
2014-01-30 22:46:08 +0100

    Created some methods.
    Signed-off-by: stroju <stroju@gmail.com>

--- Lottery.php
+++ Lottery.php
@@ -1,12 +1,55 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Rafal Strojek <strojek.rafal@gmail.com>
- *
+ /*
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
- 
+
+ /**
+ * Lottery class
+ * @author     	Rafal Strojek <strojek.rafal@gmail.com>
+ * @copyright  	2014 (c) Rafal Strojek
+ * @version		0.1
+ */
+
+class Lottery
+{
+	/**
+	 * Default parameters
+	 */
+	private $params = array();
+	
+	/**
+	 * Constructor
+	 *
+	 * @param	array  $params  User-defined parameters
+	 */
+	public function __construct(array $params)
+	{
+		$this->params = array_merge($this->getDefaultParameters(),$params);
+	}
+	
+	/**
+	 * Gets default parameters
+	 *
+	 * @return  array  Default Parameters
+	 */
+	public function getDefaultParameters()
+	{
+		return array(
+			'from' => 1, 
+			'to' => 49,
+			'seed' => mt_rand(),
+		);
+	}
+	
+	/**
+	 * Gets parameters
+	 *
+	 * @return  array  Lottery parameters
+	 */
+	public function getParameters()
+	{
+		return $this->params;
+	}
+}
