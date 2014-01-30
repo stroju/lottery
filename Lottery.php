c94a6b2eccc791d6b5d283dd4b06b88daae9f179
stroju <stroju@gmail.com>
2014-01-30 22:14:02 +0100

    Signed-off-by: stroju <stroju@gmail.com>

--- Lottery.php
+++ Lottery.php
@@ -1,1 +1,12 @@
 <?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Rafal Strojek <strojek.rafal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+ 
