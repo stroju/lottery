886156867bfe84507467ee7048eee1cf3282d93f
stroju <stroju@gmail.com>
2014-01-31 11:28:02 +0100

    Added random generator and some methods
    Signed-off-by: stroju <stroju@gmail.com>

--- Lottery.php
+++ Lottery.php
@@ -7,9 +7,10 @@
 
  /**
  * Lottery class
- * @author     	Rafal Strojek <strojek.rafal@gmail.com>
- * @copyright  	2014 (c) Rafal Strojek
- * @version		0.1
+ *
+ * @author      Rafal Strojek <strojek.rafal@gmail.com>
+ * @copyright   2014 (c) Rafal Strojek
+ * @version     0.1
  */
 
 class Lottery
@@ -20,13 +21,21 @@ class Lottery
 	private $params = array();
 	
 	/**
+	 * Numbers to drawn
+	 */
+	private $numbers = array();
+	
+	/**
 	 * Constructor
 	 *
-	 * @param	array  $params  User-defined parameters
+	 * @param   array  $params  User-defined parameters
 	 */
-	public function __construct(array $params)
+	public function __construct($params = array())
 	{
 		$this->params = array_merge($this->getDefaultParameters(),$params);
+		$this->numbers = range($this->params['from'], $this->params['to'], 1);
+		
+		$this->seedRand();
 	}
 	
 	/**
@@ -39,7 +48,9 @@ class Lottery
 		return array(
 			'from' => 1, 
 			'to' => 49,
-			'seed' => mt_rand(),
+			'numbers' => 6,
+			'seed' => (int) ((float) microtime() * 1000000),
+			'pow' => pow(2,24),
 		);
 	}
 	
@@ -51,5 +62,65 @@ class Lottery
 	public function getParameters()
 	{
 		return $this->params;
+	}
+	
+	public function getSeed()
+	{
+		return (int) $this->params['seed'];
+	}
+	
+	private function setSeed($seed = null)
+	{
+		$this->params['seed'] = ($seed) ? $seed : $this->makeSeed();
+
+		// Return instance to shortcut
+		return $this;
+	}
+	
+	private function seedRand()
+	{
+		mt_srand($this->getSeed());
+	}
+	
+	private function makeSeed()
+	{
+		return (int) ((mt_rand() + ((float) microtime() * 1000000)) % $this->params['pow']);
+	}
+	
+	public function createTicket()
+	{
+		$ticket = array();
+		$array = $this->numbers;
+		
+		for($i = 0; $i < $this->params['numbers']; $i++)
+		{
+			$this->setSeed()->seedRand();
+			$key = mt_rand(0, (count($array) - 1));
+			
+			$ticket[$i] = $array[$key];
+			array_splice($array, $key, 1);
+		}
+		
+		asort($ticket);
+		
+		return $ticket;
+	}
+	
+	public function createTickets($count = null)
+	{
+		if($count <= 0)
+		{
+			return array();
+		}
+		
+		$count = min(500, max(1, $count));
+		$tickets = array();
+		for($i = 0; $i < $count; $i++)
+		{
+			$tickets[$i] = $this->createTicket();
+		}
+		
+		return $tickets;
+		
 	}
 }
